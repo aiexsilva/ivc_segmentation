28fa1d1e1a26d611bef9cd9267c21293b00fdc02
diana <99146180+aiexsilva@users.noreply.github.com>
2024-10-30 16:15:55 +0000

    fully functional
    separated segmentation from game

--- segmentation/main.py
+++ segmentation/main.py
@@ -1,6 +1,8 @@
 import pygame, sys, random
 import cv2
 import numpy as np
+import segmentation_function
+from segmentation_function import open_camera, segmentation, contours_main, contours2_main
 
 pygame.init()
 
@@ -31,56 +33,14 @@ ball.center = (WIDTH / 2, HEIGHT / 2)
 x_speed, y_speed = 1, 1
 
 #stating camera
-capture = cv2.VideoCapture(0)
+capture = open_camera()
 
 while True:
-    ret, frame = capture.read()
-    #making it so the camera is not mirrored
-    frame = cv2.flip(frame, 1)
 
-    if not ret:
-        print("not able to grab frame")
-        continue
-
-    #makes it so the frame goes from rgb to hsv
-    hsv = cv2.cvtColor(frame, cv2.COLOR_BGR2HSV)
-
-    #lower threshold for green
-    lower_green = np.array([35, 100, 50])
-    #upper threshold for green
-    upper_green = np.array([85, 255, 255])
-
-    #this is for red, since red is in the beginning and end of the hsv color graph then u need 2 different ranges
-    lower_red_1 = np.array([0, 150, 150])
-    upper_red_1 = np.array([10, 255, 255])
-    lower_red_2 = np.array([170, 150, 150])
-    upper_red_2 = np.array([180, 255, 255])
-
-    #mask the objects, shows in white our color and black the rest
-    mask_green = cv2.inRange(hsv, lower_green, upper_green)
-    mask_red_1 = cv2.inRange(hsv, lower_red_1, upper_red_1)
-    mask_red_2 = cv2.inRange(hsv, lower_red_2, upper_red_2)
-
-    #combine masks
-    mask_red = mask_red_1 | mask_red_2
-
-    #shows the actual color of the object instead of just white
-    green_segmentation = cv2.bitwise_and(frame, frame, mask=mask_green)
-    blue_segmentation = cv2.bitwise_and(frame, frame, mask=mask_red)
-
-    #finds all the points that make up the contour of the "color block"
-    #cv2.RETR_EXTERNAL only gets the external contour, the largest, ignoring any internal contours
-    contours, _ = cv2.findContours(mask_green, cv2.RETR_EXTERNAL, cv2.CHAIN_APPROX_SIMPLE)
-    contours2, _ = cv2.findContours(mask_red, cv2.RETR_EXTERNAL, cv2.CHAIN_APPROX_SIMPLE)
+    frame, contours, contours2 = segmentation(capture)
 
     if contours:
-        largest_contour = max(contours, key=cv2.contourArea)
-        #creates rectangle that bounds the largest contour found
-        x, y, w, h = cv2.boundingRect(largest_contour)
-        cv2.rectangle(frame, (x, y), (x + w, y + h), (0, 255, 0), 2)
-
-        #calculates center of that rectangle
-        green_center = (x + w // 2, y + h // 2)
+        green_center = contours_main(frame, contours)
         #defines the center of that rectangle as the player object
         player.centery = green_center[1]
 
@@ -91,11 +51,7 @@ while True:
             player.bottom = HEIGHT
 
     if contours2:
-        largest_contour_red = max(contours2, key=cv2.contourArea)
-        a, b, c, d = cv2.boundingRect(largest_contour_red)
-        cv2.rectangle(frame, (a, b), (a + c, b + d), (0, 0, 255), 2)
-
-        red_center = (a + c // 2, b + d // 2)
+        red_center = contours2_main(frame, contours2)
         opponent.centery = red_center[1]
         if opponent.top < 0:
             opponent.top = 0
@@ -137,12 +93,9 @@ while True:
     SCREEN.blit(opponent_score_text, (WIDTH / 2 - 50, 50))
 
     pygame.display.update()
-    #show all windows in between the process
-    cv2.imshow("green hsv", mask_green)
-    cv2.imshow("red hsv", mask_red)
-    cv2.imshow("og cam feed", frame)
-    cv2.imshow("green segmentation", green_segmentation)
-    cv2.imshow("red segmentation", blue_segmentation)
+
+    if frame is not None:
+        cv2.imshow("og cam feed", frame)
 
     for event in pygame.event.get():
         if event.type == pygame.QUIT:
